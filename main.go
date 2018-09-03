ae047b7574458a373f60c3a090a0e4c2f15bb650
Alexey Andreev <bezumny.tochk@gmail.com>
2018-09-03 16:53:35 +0400

    Fixes

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	log "github.com/Sirupsen/logrus"
 	"github.com/kidoman/embd"
 	"github.com/kidoman/embd/controller/pca9685"
+	_ "github.com/kidoman/embd/host/rpi"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	pwm := pca9685.New(embd.NewI2CBus(0), 0)
-	err := pwm.SetPwm(1, 2048, 2048)
-	if err != nil {
-		log.Error(err)
+	bus := embd.NewI2CBus(1)
+	pwm := pca9685.New(bus, 0x40)
+	for i := 0; i < 3; i++ {
+		err := pwm.SetPwm(0 + (i * 4), 0, 4000)
+		if err != nil {
+			log.Error(err)
+		}
+		err = pwm.SetPwm(1 + (i * 4), 0, 4000)
+		if err != nil {
+			log.Error(err)
+		}
+		err = pwm.SetPwm(2 + (i * 4), 0, 4000)
+		if err != nil {
+			log.Error(err)
+		}
+		err = pwm.SetPwm(3 + (i * 4), 0, 4000)
+		if err != nil {
+			log.Error(err)
+		}
 	}
 }
