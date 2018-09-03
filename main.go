4c200990203c41f3ff645934a25b501e747a9b0d
Alexey Andreev <bezumny.tochk@gmail.com>
2018-09-03 19:36:10 +0400

    Using Cie1931

--- main.go
+++ main.go
@@ -14,11 +14,25 @@ import (
 	"github.com/tochk/led_strip/templates"
 )
 
+const ledMax = 4095
+
+func Cie1931(b float64) float32 {
+	b /= ledMax
+	b *= 100
+	if b <= 8 {
+		return float32(b / 902.3) * ledMax
+	}
+	x := (float64(b) + 16) / 116
+	x = x * x * x
+	return float32(x) * ledMax
+}
+
+
 type ledData struct {
-	white int
-	red   int
-	green int
-	blue  int
+	white float64
+	red   float64
+	green float64
+	blue  float64
 }
 
 var (
@@ -26,7 +40,7 @@ var (
 )
 
 func alertHandler(w http.ResponseWriter, r *http.Request) {
-	alert()
+	go alert()
 }
 
 func (data *ledData) indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,19 +49,19 @@ func (data *ledData) indexHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseForm()
 		var err error
-		data.white, err = strconv.Atoi(r.PostForm.Get("white"))
+		data.white, err = strconv.ParseFloat(r.PostForm.Get("white"), 64)
 		if err != nil {
 			log.Error(err)
 		}
-		data.red, err = strconv.Atoi(r.PostForm.Get("red"))
+		data.red, err = strconv.ParseFloat(r.PostForm.Get("red"), 64)
 		if err != nil {
 			log.Error(err)
 		}
-		data.green, err = strconv.Atoi(r.PostForm.Get("green"))
+		data.green, err = strconv.ParseFloat(r.PostForm.Get("green"), 64)
 		if err != nil {
 			log.Error(err)
 		}
-		data.blue, err = strconv.Atoi(r.PostForm.Get("blue"))
+		data.blue, err = strconv.ParseFloat(r.PostForm.Get("blue"), 64)
 		if err != nil {
 			log.Error(err)
 		}
@@ -61,19 +75,19 @@ func (data *ledData) apply() {
 	bus := embd.NewI2CBus(1)
 	pwm := pca9685.New(bus, 0x40)
 	for i := 0; i < 4; i++ {
-		err := pwm.SetPwm(0+(i*4), 0, data.white)
+		err := pwm.SetPwm(0+(i*4), 0, int(Cie1931(data.white)))
 		if err != nil {
 			log.Error(err)
 		}
-		err = pwm.SetPwm(1+(i*4), 0, data.red)
+		err = pwm.SetPwm(1+(i*4), 0, int(Cie1931(data.red)))
 		if err != nil {
 			log.Error(err)
 		}
-		err = pwm.SetPwm(2+(i*4), 0, data.green)
+		err = pwm.SetPwm(2+(i*4), 0, int(Cie1931(data.green)))
 		if err != nil {
 			log.Error(err)
 		}
-		err = pwm.SetPwm(3+(i*4), 0, data.blue)
+		err = pwm.SetPwm(3+(i*4), 0, int(Cie1931(data.blue)))
 		if err != nil {
 			log.Error(err)
 		}
